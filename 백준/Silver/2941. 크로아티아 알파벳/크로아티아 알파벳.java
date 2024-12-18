ccc3e5f970ffa5c9bda9f7046af7633cc9731a7d
seoka_moto <82501180+tjrgk1333@users.noreply.github.com>
2024-12-18 10:33:39 +0900

    Update 크로아티아 알파벳.java

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -11,13 +11,9 @@ public class Main {
         for (int i = 0; i < croAlpha.length(); i++) {
             if (croAlpha.charAt(i) == '=') {
                 if ((i - 1) >= 0) {
-                    if (croAlpha.charAt(i - 1) == 'c') {
-                        croNum++;
-                    } else if (croAlpha.charAt(i - 1) == 's') {
-                        croNum++;
-                    } else if (croAlpha.charAt(i - 1) == 'z') {
+                    if (croAlpha.charAt(i - 1) == 'c' || croAlpha.charAt(i - 1) == 's' || croAlpha.charAt(i - 1) == 'z') {
                         if ((i - 2) >= 0) {
-                            if (croAlpha.charAt(i - 2) == 'd') {
+                            if (croAlpha.charAt(i-1) == 'z' && croAlpha.charAt(i - 2) == 'd') {
                                 croNum += 2;
                             } else {
                                 croNum++;
@@ -28,24 +24,20 @@ public class Main {
                     }
                 }
             } else if (croAlpha.charAt(i) == '-') {
-                if ((i-1) >= 0){
-                    if (croAlpha.charAt(i-1) == 'c'){
-                        croNum++;
-                    } else if (croAlpha.charAt(i-1) == 'd') {
+                if ((i - 1) >= 0) {
+                    if (croAlpha.charAt(i - 1) == 'c' || croAlpha.charAt(i - 1) == 'd') {
                         croNum++;
                     }
                 }
             } else if (croAlpha.charAt(i) == 'j') {
-                if ((i-1) >= 0){
-                    if (croAlpha.charAt(i-1) == 'n') {
-                        croNum++;
-                    } else if (croAlpha.charAt(i-1) == 'l') {
+                if ((i - 1) >= 0) {
+                    if (croAlpha.charAt(i - 1) == 'n' || croAlpha.charAt(i - 1) == 'l') {
                         croNum++;
                     }
                 }
             }
         }
 
-        System.out.println(croAlpha.length()-croNum);
+        System.out.println(croAlpha.length() - croNum);
     }
 }
