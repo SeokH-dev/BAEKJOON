aa8f6408c8ef3553b8f72b8ca7a3b0c3e044c00e
seoka_moto <82501180+SeokH-dev@users.noreply.github.com>
2025-01-15 23:21:21 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testScore = sc.nextInt();
+
+        if (testScore >= 90) {
+            System.out.println("A");
+        } else if (testScore >= 80) {
+            System.out.println("B");
+        } else if (testScore >= 70) {
+            System.out.println("C");
+        } else if (testScore >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
